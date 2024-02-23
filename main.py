942313a1fea64fdcb0bc7cf20aadeb752e85da59
Y3fri <yeferguzman11@gmail.com>
2024-02-23 16:00:04 -0500

    Agrege el CORS

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from config.database import engine,Base
 from middlewares.error_handler import ErrorHandler
 from routers.empresa import empresa_router
@@ -15,6 +16,20 @@ app = FastAPI(
     title= 'prendiendo FastApi',
     description= 'Una API ',
     version= '0.0.2',
+)
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000", 
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.add_middleware(ErrorHandler)
